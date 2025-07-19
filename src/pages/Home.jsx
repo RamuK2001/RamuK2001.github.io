08dec303e880f11d7c6127a7f175b6e00b880367
RamuK2001 <85925640+RamuK2001@users.noreply.github.com>
2025-07-19 16:47:32 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <section className="p-10 bg-gradient-to-r from-blue-100 to-purple-100 min-h-[60vh] text-center">
-      <h2 className="text-4xl font-bold text-blue-900 mb-4">Cloud Data Engineer – Infra to Insights</h2>
+      <h2 className="text-4xl font-bold text-blue-900 mb-4">Cloud Data Engineer | Infra to Insights</h2>
       <p className="text-lg max-w-2xl mx-auto text-gray-700">
         Results-driven Azure Data Engineer with 3 years of experience in ETL Development, Data Engineering, and Azure DevOps Automation. Adept at designing and implementing Azure-based ETL solutions, developing CI/CD pipelines, and driving cost-efficient cloud operations. Expertise in Azure Databricks (ADB), Azure Data Factory (ADF), DevOps, ETL, and Data Warehousing.
       </p>
