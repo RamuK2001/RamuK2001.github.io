b48ff292be473dc49ad595f9aa034ecab24b65cc
RamuK2001 <ramukarnati2001@gmail.com>
2026-04-11 22:02:34 +0530

    set default dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+
+function setDefaultDarkMode() {
+  // Always set dark mode on initial load
+  document.documentElement.classList.add("dark");
+  // If you use localStorage for theme, set it as well:
+  localStorage.setItem("theme", "dark");
+}
+
+// Call this before your app renders
+setDefaultDarkMode();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
